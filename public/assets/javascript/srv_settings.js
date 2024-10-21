e0f8b722ef0379113e55a5062ea75eea06c4dc4e
rxfe-a <28RafaelA539@gmail.com>
2024-10-21 00:35:25 -0700

    uhh ion know what I did

--- public/assets/javascript/srv_settings.js
+++ public/assets/javascript/srv_settings.js
@@ -1,2 +1,14 @@
-var forceproxyoverride_check = document.getElementById("forceproxyoverride_check");
-var proxymode = document.getElementById("pm");
+var currentConfig = localStorage.getItem("LocalSettings")
+var StorageParsed = JSON.parse(currentConfig);
+
+function changeval(key, value) {
+    StorageParsed[key] = value; 
+    console.log(`changed ${key} with ${value}`);
+}
+
+document.getElementById("savebutton_proxy").onclick = function () {
+    changeval('proxy', document.getElementById('pm').value);
+    changeval('force_proxyoverride', document.getElementById('forceproxyoverride_check').value);
+
+    localStorage.setItem('LocalSettings', JSON.stringify(StorageParsed)); 
+}
